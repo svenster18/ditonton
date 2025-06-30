4168897fd99e50cc14f210132bbaedfc2db3a9c1
Mohamad Rizki <mohamadrizki8@gmail.com>
2025-06-30 10:16:59 +0700

    test: Add unit test for TvSeriesModel
    This commit adds a unit test for the `TvSeriesModel` to verify that its `toEntity()` method correctly converts it to a `TvSeries` entity.

---
+++ test/data/models/tv_series_response_model_test.dart
@@ -0,0 +1,40 @@
+import 'package:ditonton/data/models/tv_series_model.dart';
+import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final tTvSeriesModel = TvSeriesModel(
+      backdropPath: 'backdropPath',
+      genreIds: [1, 2, 3],
+      id: 1,
+      originCountry: ['originCountry'],
+      originalLanguage: 'originalLanguage',
+      originalName: 'originalName',
+      overview: 'overview',
+      popularity: 1,
+      posterPath: 'posterPath',
+      firstAirDate: 'firstAirDate',
+      name: 'name',
+      voteAverage: 1,
+      voteCount: 1);
+
+  final tTvSeries = TvSeries(
+      backdropPath: 'backdropPath',
+      genreIds: [1, 2, 3],
+      id: 1,
+      originCountry: ['originCountry'],
+      originalLanguage: 'originalLanguage',
+      originalName: 'originalName',
+      overview: 'overview',
+      popularity: 1,
+      posterPath: 'posterPath',
+      firstAirDate: 'firstAirDate',
+      name: 'name',
+      voteAverage: 1,
+      voteCount: 1);
+
+  test('should be a subclass of TvSeries entity', () async {
+    final result = tTvSeriesModel.toEntity();
+    expect(result, tTvSeries);
+  });
+}
