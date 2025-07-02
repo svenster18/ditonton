bfb4276bf86e48229c9636ae2f11d72660cfa121
Mohamad Rizki <mohamadrizki8@gmail.com>
2025-07-02 23:54:27 +0700

    feat: Implement TopRatedTvSeriesNotifier and unit tests
    This commit introduces the `TopRatedTvSeriesNotifier` for managing the state of top-rated TV series and its corresponding unit tests.
    
    It includes:
    - `TopRatedTvSeriesNotifier`:
      - Constructor takes `GetTopRatedTvSeries` as a dependency.
      - `fetchTopRatedTvSeries` method to fetch top-rated TV series, updating state to Loading, Loaded, or Error.
    - Unit tests for `TopRatedTvSeriesNotifier`:
      - Verifies state changes to Loading when fetching data.
      - Verifies successful data fetching and state update to Loaded.
      - Verifies error handling and state update to Error.
    - Mocks for `GetTopRatedTvSeries`.

--- lib/presentation/provider/top_rated_tv_series_notifier.dart
+++ lib/presentation/provider/top_rated_tv_series_notifier.dart
@@ -4,8 +4,13 @@ import 'package:flutter/material.dart';
 
 import '../../common/state_enum.dart';
 import '../../domain/entities/tv_series.dart';
+import '../../domain/usecases/get_top_rated_tv_series.dart';
 
 class TopRatedTvSeriesNotifier extends ChangeNotifier {
+  final GetTopRatedTvSeries getTopRatedTvSeries;
+
+  TopRatedTvSeriesNotifier({required this.getTopRatedTvSeries});
+
   RequestState _state = RequestState.Empty;
   RequestState get state => _state;
 
@@ -15,5 +20,18 @@ class TopRatedTvSeriesNotifier extends ChangeNotifier {
   String _message = '';
   String get message => _message;
 
-  Future<void> fetchTopRatedTvSeries() async {}
+  Future<void> fetchTopRatedTvSeries() async {
+    _state = RequestState.Loading;
+    notifyListeners();
+    final result = await getTopRatedTvSeries.execute();
+    result.fold((failure) {
+      _message = failure.message;
+      _state = RequestState.Error;
+      notifyListeners();
+    }, (tvSeriesData) {
+      _tvSeries = tvSeriesData;
+      _state = RequestState.Loaded;
+      notifyListeners();
+    });
+  }
 }
