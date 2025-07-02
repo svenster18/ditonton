18f71de2f1171d6826d6c0d7a2aee847536f8f41
Mohamad Rizki <mohamadrizki8@gmail.com>
2025-07-02 23:45:51 +0700

    feat: Add top-rated TV series presentation layer
    This commit introduces the presentation layer for top-rated TV series.
    
    It includes:
    - `TopRatedTvSeriesNotifier` for managing the state of top-rated TV series.
    - `TopRatedTvSeriesPage` to display the list of top-rated TV series.
    - Unit tests for `TopRatedTvSeriesPage` to verify loading, loaded, and error states.
    - Mocks for `TopRatedTvSeriesNotifier`.
    
    The `fetchTopRatedTvSeries` method in the notifier is currently empty and requires implementation.

---
+++ lib/presentation/provider/top_rated_tv_series_notifier.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../../common/state_enum.dart';
+import '../../domain/entities/tv_series.dart';
+
+class TopRatedTvSeriesNotifier extends ChangeNotifier {
+  RequestState _state = RequestState.Empty;
+  RequestState get state => _state;
+
+  List<TvSeries> _tvSeries = [];
+  List<TvSeries> get tvSeries => _tvSeries;
+
+  String _message = '';
+  String get message => _message;
+
+  Future<void> fetchTopRatedTvSeries() async {}
+}
