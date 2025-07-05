5546b4cd9abec20a8c76e135655d892242032276
Mohamad Rizki <mohamadrizki8@gmail.com>
2025-07-05 23:18:05 +0700

    feat: Implement "Remove TV Series Watchlist" use case
    This commit introduces the `RemoveTvSeriesWatchlist` use case, which allows removing a TV series from the user's watchlist.
    
    It includes:
    - `RemoveTvSeriesWatchlist` class with an `execute` method that takes a `TvSeriesDetail` object and interacts with the `TvSeriesRepository`.
    - Unit tests for `RemoveTvSeriesWatchlist` to verify that it correctly calls the repository's `removeWatchlist` method.
    - Mocks for `TvSeriesRepository`.

--- lib/domain/usecases/remove_tv_series_watchlist.dart
+++ lib/domain/usecases/remove_tv_series_watchlist.dart
@@ -1,5 +1,15 @@
+import 'package:dartz/dartz.dart';
 import 'package:ditonton/domain/entities/tv_series_detail.dart';
 
+import '../../common/failure.dart';
+import '../repositories/tv_series_repository.dart';
+
 class RemoveTvSeriesWatchlist {
-  execute(TvSeriesDetail tvSeries) {}
+  final TvSeriesRepository repository;
+
+  RemoveTvSeriesWatchlist(this.repository);
+
+  Future<Either<Failure, String>> execute(TvSeriesDetail tvSeries) {
+    return repository.removeWatchlist(tvSeries);
+  }
 }
