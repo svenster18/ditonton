d7e286b656179ef8cf60afcb2f7356fee115f231
Mohamad Rizki <mohamadrizki8@gmail.com>
2025-12-29 11:46:10 +0700

    Refactor: Update API key in remote data source test
    This commit updates the `API_KEY` constant in the `movie_remote_data_source_test.dart` file to a new value.

--- test/data/datasources/movie_remote_data_source_test.dart
+++ test/data/datasources/movie_remote_data_source_test.dart
@@ -12,7 +12,7 @@ import '../../json_reader.dart';
 import '../../helpers/test_helper.mocks.dart';
 
 void main() {
-  const API_KEY = 'api_key=2174d146bb9c0eab47529b2e77d6b526';
+  const API_KEY = 'api_key=0ef68cbe1d05aed4015f6d6c0c896805';
   const BASE_URL = 'https://api.themoviedb.org/3';
 
   late MovieRemoteDataSourceImpl dataSource;
