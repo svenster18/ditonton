93e0b68e557358f2557ead74b2ae6c182fec60e0
Mohamad Rizki <mohamadrizki8@gmail.com>
2025-12-30 10:00:13 +0700

    refactor: Enhance and refactor movie detail BLoC tests
    This commit refactors the `movie_detail_bloc_test.dart` file to improve test organization and coverage.
    
    Key changes include:
    - Consolidating tests for fetching movie details and recommendations into a single group.
    - Removing an unused import of `movie_detail.dart`.
    - Adding comprehensive test cases for the `Watchlist` functionality, including:
      - Removing an item from the watchlist.
      - Handling both success and failure scenarios when adding an item.
      - Verifying that the watchlist status is correctly updated.
    - Adding a new test group to handle error scenarios during data fetching, such as when fetching movie details fails.

--- test/presentation/bloc/movie_detail_bloc_test.dart
+++ test/presentation/bloc/movie_detail_bloc_test.dart
@@ -2,7 +2,6 @@ import 'package:bloc_test/bloc_test.dart';
 import 'package:dartz/dartz.dart';
 import 'package:ditonton/common/failure.dart';
 import 'package:ditonton/domain/entities/movie.dart';
-import 'package:ditonton/domain/entities/movie_detail.dart';
 import 'package:ditonton/domain/usecases/get_movie_detail.dart';
 import 'package:ditonton/domain/usecases/get_movie_recommendations.dart';
 import 'package:ditonton/domain/usecases/get_watchlist_status.dart';
@@ -74,7 +73,7 @@ void main() {
         .thenAnswer((_) async => Right(tMovies));
   }
 
-  group('Get Movie Detail', () {
+  group('Get Movie Detail and Recommendations', () {
     blocTest<MovieDetailBloc, MovieDetailState>(
         'should get data from the usecase',
         build: () {
@@ -82,20 +81,25 @@ void main() {
           return movieDetailBloc;
         },
         act: (bloc) => bloc.add(OnFetchMovieDetail(tId)),
-        wait: const Duration(milliseconds: 500),
+        verify: (bloc) {
+          verify(mockGetMovieDetail.execute(tId));
+          verify(mockGetMovieRecommendations.execute(tId));
+        });
+
+    blocTest<MovieDetailBloc, MovieDetailState>(
+        'should change movie and recommendations when data is gotten successfully',
+        build: () {
+          _arrangeUsecase();
+          return movieDetailBloc;
+        },
+        act: (bloc) => bloc.add(OnFetchMovieDetail(tId)),
         expect: () => [
               MovieDetailLoading(),
               MovieRecommendationsLoading(),
               MovieRecommendationsHasData(tMovies),
               MovieDetailHasData(testMovieDetail),
-            ],
-        verify: (bloc) {
-          verify(mockGetMovieDetail.execute(tId));
-          verify(mockGetMovieRecommendations.execute(tId));
-        });
-  });
+            ]);
 
-  group('Get Movie Recommendation', () {
     blocTest<MovieDetailBloc, MovieDetailState>(
         'should update error message when request in successful',
         build: () {
@@ -152,7 +156,78 @@ void main() {
             ],
         verify: (bloc) {
           verify(mockSaveWatchlist.execute(testMovieDetail));
+        });
+
+    blocTest<MovieDetailBloc, MovieDetailState>(
+        'should execute remove watchlist when function called',
+        build: () {
+          when(mockRemoveWatchlist.execute(testMovieDetail))
+              .thenAnswer((_) async => Right('Removed'));
+          when(mockGetWatchlistStatus.execute(testMovieDetail.id))
+              .thenAnswer((_) async => false);
+          return movieDetailBloc;
+        },
+        act: (bloc) => bloc.add(OnRemoveWatchlist(testMovieDetail)),
+        wait: const Duration(milliseconds: 500),
+        expect: () => <MovieDetailState>[
+              WatchlistSuccess('Removed'),
+              WatchlistStatusLoaded(false),
+            ],
+        verify: (bloc) {
+          verify(mockRemoveWatchlist.execute(testMovieDetail));
+        });
+
+    blocTest<MovieDetailBloc, MovieDetailState>(
+        'should update watchlist status when add watchlist success',
+        build: () {
+          when(mockSaveWatchlist.execute(testMovieDetail))
+              .thenAnswer((_) async => Right('Added to Watchlist'));
+          when(mockGetWatchlistStatus.execute(testMovieDetail.id))
+              .thenAnswer((_) async => true);
+          return movieDetailBloc;
+        },
+        act: (bloc) => bloc.add(OnAddWatchlist(testMovieDetail)),
+        wait: const Duration(milliseconds: 500),
+        expect: () => <MovieDetailState>[
+              WatchlistSuccess('Added to Watchlist'),
+              WatchlistStatusLoaded(true),
+            ],
+        verify: (bloc) {
           verify(mockGetWatchlistStatus.execute(testMovieDetail.id));
         });
+
+    blocTest<MovieDetailBloc, MovieDetailState>(
+        'should update watchlist message when add watchlist failed',
+        build: () {
+          when(mockSaveWatchlist.execute(testMovieDetail))
+              .thenAnswer((_) async => Left(DatabaseFailure('Failed')));
+          when(mockGetWatchlistStatus.execute(testMovieDetail.id))
+              .thenAnswer((_) async => true);
+          return movieDetailBloc;
+        },
+        act: (bloc) => bloc.add(OnAddWatchlist(testMovieDetail)),
+        wait: const Duration(milliseconds: 500),
+        expect: () => <MovieDetailState>[
+              WatchlistError('Failed'),
+              WatchlistStatusLoaded(true),
+            ]);
+  });
+
+  group('on Error', () {
+    blocTest<MovieDetailBloc, MovieDetailState>(
+        'should return error when data is unsuccessful',
+        build: () {
+          when(mockGetMovieDetail.execute(tId))
+              .thenAnswer((_) async => Left(ServerFailure('Server Failed')));
+          when(mockGetMovieRecommendations.execute(tId))
+              .thenAnswer((_) async => Right(tMovies));
+          return movieDetailBloc;
+        },
+        act: (bloc) => bloc.add(OnFetchMovieDetail(tId)),
+        wait: const Duration(milliseconds: 500),
+        expect: () => <MovieDetailState>[
+              MovieDetailLoading(),
+              MovieDetailError('Server Failure'),
+            ]);
   });
 }
